b55dba1e5d3b86ab9feed1210d501a4e1984fc0a
svasenkov <aanher@gmail.com>
2023-10-25 19:46:27 +0200

    added simple test

---
+++ tests/simple/test_fail.py
@@ -0,0 +1,17 @@
+def test_fail1():
+    assert False
+
+def test_fail2():
+    assert False
+
+def test_fail3():
+    assert False
+
+def test_fail4():
+    assert False
+
+def test_fail5():
+    assert False
+
+def test_fail6():
+    assert False
